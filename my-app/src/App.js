05145034479c267654541c690ef15a1ac515efa5
Stuistheman <Stuistheman@gmail.com>
2024-02-06 12:11:17 -0600

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { AboutPage } from './tests';
 
 
 function App() {
   return (
     <div className="App">
-      
-
-      <h1> hello </h1>
+      <h1>bob</h1>
+      <h1></h1>
+      <AboutPage/>
 
 
 
